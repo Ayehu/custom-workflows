b89c4308ac708e6d0a4b8757516f44ba55dcec9c
Derek Pascarella <contact.pascarella@gmail.com>
2019-08-21 12:41:22 -0400

    Update workflow_activity_counter.ps1

--- workflow_activity_counter.ps1
+++ workflow_activity_counter.ps1
@@ -1,4 +1,10 @@
-# PowerShell implementation of the Perl script "workflow_activity_counter.pl" that prints only the total activity count.
+# PowerShell implementation of the Perl script "workflow_activity_counter.pl" that prints only the total activity count.  At the PowerShell
+# prompt, this script would be executed as follows:
+#
+# > C:\full\path\to\workflow_activity_counter.ps1 C:\full\path\to\WorkFlow.xml
+#
+# Sample output:
+# Total activities: 4
 
 $workflow_file = $args[0]
 
